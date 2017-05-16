29a779fc23854ca38d3f6da5ca803874b1903d28
akm2017 <amitmitra83@gmail.com>
2017-05-16 09:24:20 +0530

    Note entities f

--- noteanalyzer/src/main/java/com/noteanalyzer/entity/notes/Property.java
+++ noteanalyzer/src/main/java/com/noteanalyzer/entity/notes/Property.java
@@ -1,13 +1,17 @@
 package com.noteanalyzer.entity.notes;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import com.noteanalyzer.entity.AbstractEntity;
+import com.noteanalyzer.entity.address.Area;
 
 import lombok.ToString;
  
@@ -27,12 +31,53 @@ public class Property extends AbstractEntity {
     @Column(name="PROP_ID")
     private Integer propertyId;
  
-    @Column(name="PROP_NAME")
+	@ManyToOne(optional = false)
+	@JoinColumn(name="AREA_ID")
+	private Area area;
+	
+	@Column(name="PROP_NAME")
     private String propertyName;
  
-    @Column(name="PROP_DESC")
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "PROPERTY_TYPE")
+	private PropertyType propertyType;
+	
+	@Column(name="PROP_DESC")
     private String propertyDescription;
+	
+	
+	@Column(name="STREET")
+	private String street;
+	
+	@Column(name="CITY")
+	private String	city;
+	
+	@Column(name="STATE")
+	private String	state;
+	
+	@Column(name="ZIP")
+	private Integer	Zip;
+	
+	@Column(name="AGE")
+	private Integer	age;
+	
+	@Column(name="SIZE_SF")
+	private Integer	size_sf;
+	
+	@Column(name="SUBDIVIDABLE")
+	private boolean subdividable;
+	
+	@Column(name="OTH_HIGH_PRIO_DEBT")
+	private Integer	otherHigherPriorityDebt;
+	
+	@Column(name="OTH_LOW_PRIO_DEBT")
+	private Integer OtherLowerPriorityDebt;
+	
+	@Column(name="OTH_MONTH_EXPENSE")
+	private Integer otherMonthlyExpenses;
 
+	
+	
 	public Integer getPropertyId() {
 		return propertyId;
 	}
@@ -55,6 +100,104 @@ public class Property extends AbstractEntity {
 
 	public void setPropertyDescription(String propertyDescription) {
 		this.propertyDescription = propertyDescription;
+	}
+
+	
+	
+	public Area getArea() {
+		return area;
+	}
+
+	public void setArea(Area area) {
+		this.area = area;
+	}
+
+	public PropertyType getPropertyType() {
+		return propertyType;
+	}
+
+	public void setPropertyType(PropertyType propertyType) {
+		this.propertyType = propertyType;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Integer getZip() {
+		return Zip;
+	}
+
+	public void setZip(Integer zip) {
+		Zip = zip;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getSize_sf() {
+		return size_sf;
+	}
+
+	public void setSize_sf(Integer size_sf) {
+		this.size_sf = size_sf;
+	}
+
+	public boolean isSubdividable() {
+		return subdividable;
+	}
+
+	public void setSubdividable(boolean subdividable) {
+		this.subdividable = subdividable;
+	}
+
+	public Integer getOtherHigherPriorityDebt() {
+		return otherHigherPriorityDebt;
+	}
+
+	public void setOtherHigherPriorityDebt(Integer otherHigherPriorityDebt) {
+		this.otherHigherPriorityDebt = otherHigherPriorityDebt;
+	}
+
+	public Integer getOtherLowerPriorityDebt() {
+		return OtherLowerPriorityDebt;
+	}
+
+	public void setOtherLowerPriorityDebt(Integer otherLowerPriorityDebt) {
+		OtherLowerPriorityDebt = otherLowerPriorityDebt;
+	}
+
+	public Integer getOtherMonthlyExpenses() {
+		return otherMonthlyExpenses;
+	}
+
+	public void setOtherMonthlyExpenses(Integer otherMonthlyExpenses) {
+		this.otherMonthlyExpenses = otherMonthlyExpenses;
 	}
 
 	@Override
--- noteanalyzer/src/main/java/com/noteanalyzer/entity/valuation/BorrowerType.java
+++ noteanalyzer/src/main/java/com/noteanalyzer/entity/valuation/BorrowerType.java
@@ -11,6 +11,8 @@ import com.noteanalyzer.entity.AbstractEntity;
 @Table(name="BORROWER_TYPE")
 public class BorrowerType  extends AbstractEntity {
 
+	private static final long serialVersionUID = 2057071124772914109L;
+
 	@Id
 	@Column(name="BORROWER_TYPE")
 	private String borrowerType;
