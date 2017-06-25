1cffa2545dd59c7e3c24e20fd05fb862dfbf92e0
Arvind Ray <arvind.ray84@gmail.com>
2017-06-25 21:34:17 +0530

    Note Calculator

--- noteanalyzer/src/main/webapp/static/js/service/noteService.js
+++ noteanalyzer/src/main/webapp/static/js/service/noteService.js
@@ -20,17 +20,19 @@ noteApp.factory('NoteService', ['$http', 'toastr', '$q', '$rootScope', '$uibModa
 		 var term  = noteInputFormModel.originalTerm;
 		 var interestRate   = noteInputFormModel.rate;
 		 var payment = noteInputFormModel.pdiPayment;
-		 if(principal && term && interestRate){
-			 interestRate = interestRate/1200;
-			 noteInputFormModel.pdiPayment= principal * interestRate / (1 - (Math.pow(1/(1 + interestRate), term)));
-		 } else if(principal && term && payment){
-			 noteInputFormModel.rate = calculateRate(term,payment,principal);
-		 } else if(principal && interestRate && payment){
-			 noteInputFormModel.term = getNPER(interestRate, payment, principal);
-		 }else if(term && interestRate && payment){
-			 noteInputFormModel.principal = getIPMT(interestRate, payment, term);
-		 }
-		 
+		 if(!(principal && term && interestRate && payment)) {
+			 	interestRate = interestRate / 1200;
+			 	payment = payment * -1;
+				if (principal && term && interestRate) {
+					noteInputFormModel.pdiPayment = principal * interestRate / (1 - (Math.pow(1 / (1 + interestRate), term)));
+				} else if (principal && term && payment) {
+					noteInputFormModel.rate = calculateRate(term,payment, principal) * 1200;
+				} else if (principal && interestRate && payment) {
+					noteInputFormModel.originalTerm = getNPER(interestRate, payment, principal);
+				} else if (term && interestRate && payment) {
+					noteInputFormModel.upb = getPV(interestRate, term, payment);
+				}
+				}
 		return noteInputFormModel;
 	}
 	
@@ -240,121 +242,17 @@ function compIsType(t, s) {
 		  return Math.log(num / den) / Math.log(1 + rate);
 		}
 		
-		function  getIPMT(rate, period, periods, present, future, type) {
-			  // Credits: algorithm inspired by Apache OpenOffice
+	
+	 function getPV (rate, periods, payment, future, type) {
+							  future = future || 0;
+							  type = type || 0;
 
-			  // Initialize type
-			  var type = (typeof type === 'undefined') ? 0 : type;
-
-			  // Evaluate rate and periods (TODO: replace with secure expression evaluator)
-			  rate = eval(rate);
-			  periods = eval(periods);
-
-			  // Compute payment
-			  var payment = PMT(rate, periods, present, future, type);
-			  
-			  // Compute interest
-			  var interest;
-			  if (period === 1) {
-			    if (type === 1) {
-			      interest = 0;
-			    } else {
-			      interest = -present;
-			    }
-			  } else {
-			    if (type === 1) {
-			      interest = FV(rate, period - 2, payment, present, 1) - payment;
-			    } else {
-			      interest = FV(rate, period - 1, payment, present, 0);
-			    }
-			  }
-			  
-			  // Return interest
-			  return interest * rate;
-			}
-		
-		function getIPMT (rate, period, periods, present, future, type) {
-			  // Credits: algorithm inspired by Apache OpenOffice
-
-			  future = future || 0;
-			  type = type || 0;
-
-			 
-
-			  // Compute payment
-			  var payment = getPMT(rate, periods, present, future, type);
-
-			  // Compute interest
-			  var interest;
-			  if (period === 1) {
-			    if (type === 1) {
-			      interest = 0;
-			    } else {
-			      interest = -present;
-			    }
-			  } else {
-			    if (type === 1) {
-			      interest = getFV(rate, period - 2, payment, present, 1) - payment;
-			    } else {
-			      interest = getFV(rate, period - 1, payment, present, 0);
-			    }
-			  }
-
-			  // Return interest
-			  return interest * rate;
-			};
-			
-			 function getPMT(rate, periods, present, future, type) {
-				  // Credits: algorithm inspired by Apache OpenOffice
-
-				  future = future || 0;
-				  type = type || 0;
-
-				  rate = utils.parseNumber(rate);
-				  periods = utils.parseNumber(periods);
-				  present = utils.parseNumber(present);
-				  future = utils.parseNumber(future);
-				  type = utils.parseNumber(type);
-				  if (utils.anyIsError(rate, periods, present, future, type)) {
-				    return error.value;
-				  }
-
-				  // Return payment
-				  var result;
-				  if (rate === 0) {
-				    result = (present + future) / periods;
-				  } else {
-				    var term = Math.pow(1 + rate, periods);
-				    if (type === 1) {
-				      result = (future * rate / (term - 1) + present * rate / (1 - 1 / term)) / (1 + rate);
-				    } else {
-				      result = future * rate / (term - 1) + present * rate / (1 - 1 / term);
-				    }
-				  }
-				  return -result;
-				};
-
-	function getFV (rate, periods, payment, value, type) {
-					  // Credits: algorithm inspired by Apache OpenOffice
-
-					  value = value || 0;
-					  type = type || 0;
-
-					 
-					  // Return future value
-					  var result;
-					  if (rate === 0) {
-					    result = value + payment * periods;
-					  } else {
-					    var term = Math.pow(1 + rate, periods);
-					    if (type === 1) {
-					      result = value * term + payment * (1 + rate) * (term - 1) / rate;
-					    } else {
-					      result = value * term + payment * (term - 1) / rate;
-					    }
-					  }
-					  return -result;
-					};
-
+							  // Return present value
+							  if (rate === 0) {
+							    return -payment * periods - future;
+							  } else {
+							    return (((1 - Math.pow(1 + rate, periods)) / rate) * payment * (1 + rate * type) - future) / Math.pow(1 + rate, periods);
+							  }
+							};
 }]);
 
